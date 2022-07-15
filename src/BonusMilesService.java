e07f16a57c35ba391657a8bc3d4acf4da0e116de
DanilSinitsin <sinitsindanil90@mail.ru>
2022-07-15 23:06:49 +0500

    First commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int price, int bonus) {
+int result = price / bonus;
+
+        return result;
+
+        
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 60;
-        int x = 20;
+
+       BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int bonus = 20;
+        int miles = service.calculate(10_000, 20);
         {
-            System.out.println(amount / x);
-            int mile = 3;
-            System.out.println(mile);
+            System.out.println(miles);
 
         }
 
